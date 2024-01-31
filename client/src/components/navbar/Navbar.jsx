1bdb2de965671b5a2faebf764baaebc00d420f88
Ritu Raj <rajritu142001@gmail.com>
2024-01-31 22:05:35 +0530

    Changing the title

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       <div className={classes.wrapper}>
         <div className={classes.left}>
           <Link to='/' className={classes.title}>
-            WebDevMania
+           Food Baazar 
           </Link>
         </div>
         <div className={classes.center}>
